c76b09969d919cb16fef528e65b767c4719b39a5
sidchop <sid.chopra@monash.edu>
2022-12-13 19:27:31 -0500

    .

--- test/app.R
+++ test/app.R
@@ -3,18 +3,28 @@ library(shiny)  # Required to run any Shiny app
 library(neurobase)
 library(glue)
 
+#Deploy: https://www.shinyapps.io/admin/#/dashboard
+
 # ui.R ----
 ui <- fluidPage(
   titlePanel("Visualise Voxel-level data"),
   sidebarLayout(
     sidebarPanel(
-      # Input: Select overlay template ----
+      # Input: Select  template ----
       selectInput(inputId = "template", 
-                  label = "Select brain template on which to display results:", 
+                  label = "Select .nii brain template on which to display results:", 
                   choices = c("MNI152_T1_1mm_brain","MNI152NLin2009cAsym 1mm","MNI52Lin 2mm","MNI152NLin2009cAsym 2mm"), selected = "MNI152_T1_1mm_brain"),
-      # Input: Select a file ----
+      # Input: Select overlay  ----
+      selectizeInput(inputId = 'file0', 
+                     label = 'Select .nii overlay on which to display to display on overlay:', 
+                     choices = c("Example1"),
+                     options = list(
+                       placeholder = 'Please select an option below',
+                       onInitialize = I('function() { this.setValue("NULL"); }')
+                     )),
+      # Input: Or upload a file a file ----
       fileInput(inputId = "file1", 
-                label = "Choose .nii file to overlay on brain template:",
+                label = "Or upload your own .nii overlay on which to display to display on overlay:",
                 multiple = FALSE,
                 accept = c(".gz", ".nii.gz")),
       # Horizontal line ----
@@ -48,14 +58,21 @@ server <- function(input, output) {
     readnii(paste0('/Users/sidchopra/Dropbox/Sid/R_files/RepoNeuroVis/data/',input$template, '.nii.gz'))
   })
   overlay_effect <- reactive({
-    inFile <- input$file1
-    if (is.null(inFile)) {
+    if (is.null(input$file0) && is.null(input$file1)) {
       return(NULL)
-    } else {
+    }
+    if(!is.null(input$file1) && is.null(input$file0)) {
+      reset(id = input$file0, asis = FALSE)
+      inFile <- input$file1
       # need to fix, as people might want to uplod .nii files as well. fileInput seems to strip .nii.etc # see https://github.com/tidyverse/readxl/issues/85 
       file.copy(inFile$datapath,
                 paste(inFile$datapath, ".nii.gz", sep="")) 
       mask <- readnii(paste(inFile$datapath, ".nii.gz", sep=""))
+      mask[abs(mask)<quantile(abs(mask), input$threshold)] <- NA
+      return(mask)
+    }
+    if(!is.null(input$file0) && is.null(input$file1)) {
+      mask <- readnii(paste0('data/MNI152_effect_size.nii.gz'))
       mask[abs(mask)<quantile(abs(mask), input$threshold)] <- NA
       return(mask)
     }
@@ -79,26 +96,27 @@ server <- function(input, output) {
     )  
   })
   output$code<- renderText({
-   glue("## you need the following package(s) to generate the plot:\n",
-               "library(neurobase)\n\n",
-               '## the code below read in your .nii or .nii.gz files in R:\n',
-               "template = readnii('/file/path/to/template.nii.gz')\n",
-               "overlay = readnii('/file/path/to/overlay.nii.gz')\n\n",
-               "## the code below will threshold our overlay file to top X% of voxels:\n",
-               "overlay[abs(overlay)<quantile(abs(overlay), {input$threshold})] <- NA \n\n",
-               "## the code below generates the voxel plot:\n",
-                "ortho2(x = template,   #Specify the background template   
-                 y          = overlay,    #Specify the effect you want to overlay on the template
-                 crosshairs = FALSE,      #Remove the cross-hairs
-                 bg         = 'white',    #Make the background white 
-                 NA.x       = TRUE,       #Do not display NA values
-                 col.y      = viridis::inferno(n=500), #Select the color scale. 
-                 xyz        = c({input$slice_x},{input$slice_y},{input$slice_z}), #set the x y & z slice you want to visualize
-                 useRaster  = TRUE,     #Sometimes using Raster makes for clearer plots
-                 ycolorbar  = TRUE,     #Add a color bar
-                 mfrow      =   (1,3)") #fix view
+    glue("## you need the following package(s) to generate the plot:\n",
+         "library(neurobase)\n\n",
+         '## the code below read in your .nii or .nii.gz files in R:\n',
+         '## remember to chage the file paths to where your .nii files are locally stored',
+         "template = readnii('/file/path/to/template.nii.gz')\n",
+         "overlay = readnii('/file/path/to/overlay.nii.gz')\n\n",
+         "## the code below will threshold our overlay file to top X% of voxels:\n",
+         "overlay[abs(overlay)<quantile(abs(overlay), {input$threshold})] <- NA \n\n",
+         "## the code below generates the voxel plot:\n",
+         "ortho2(  x = template,   #Specify the background template   
+               y          = overlay,    #Specify the effect you want to overlay on the template
+               crosshairs = FALSE,      #Remove the cross-hairs
+               bg         = 'white',    #Make the background white 
+               NA.x       = TRUE,       #Do not display NA values
+               col.y      = viridis::inferno(n=500), #Select the color scale. 
+               xyz        = c({input$slice_x},{input$slice_y},{input$slice_z}), #set the x y & z slice you want to visualize
+               useRaster  = TRUE,     #Sometimes using Raster makes for clearer plots
+               ycolorbar  = TRUE,     #Add a color bar
+               mfrow      =   (1,3)") #fix view
     
-    })
+  })
 }
 
 #https://github.com/gertstulp/ggplotgui/blob/master/R/ggplot_shiny.R
